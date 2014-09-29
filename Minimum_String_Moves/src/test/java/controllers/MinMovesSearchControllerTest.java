243f0e4f3836d50884817aeeae18c51234c3d8e9
dvukeli1 <mitja.vukelic@gmail.com>
2014-09-29 15:56:20 +0200

    Change-Id: I6be23387c6819cc218a469252bf625c9426cb088 Signed-off-by: dvukeli1 <mitja.vukelic@gmail.com>

--- Minimum_String_Moves/src/test/java/controllers/MinMovesSearchControllerTest.java
+++ Minimum_String_Moves/src/test/java/controllers/MinMovesSearchControllerTest.java
@@ -5,29 +5,59 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MinMovesSearchControllerTest {
+	
+	/**
+	 * First simple test
+	 */
+	
+	String s10 = "aab";
+	String s20 = "baa";
+	int result0 = 1;
+	
+	@Test
+	public void firstSimpleTest(){
+		MinMovesInterface Minmoves =  new MinMovesSearchController();
+		assertEquals("It Must be = ", result0 , Minmoves.String_mover(s10, s20));
+		
+	}
 
+	/**
+	 * Greedy algorithm starts from first char and in one direction counts 5 swaps ( 3 is correct)  in both 
+	 * directions if direction of swap gives priority to right swap if char index is less then 
+	 * half of string length.
+	 */
+	
+	
 	String s11 = "aabaaaab";
 	String s21 = "baaaaaba";
 	int result1 = 3;
 	
 	
 	@Test
-	public void testString_mover1() {
+	public void priorityDirectionCharSwapTest() {
 		MinMovesInterface Minmoves =  new MinMovesSearchController();
 		assertEquals("It Must be = ", result1 , Minmoves.String_mover(s11, s21));
 		
 	}
 	
-	String s12 = "caaaaaaaab";
-	String s22 = "cbaaaaaaaa";
-	int result2 = 3;
+	/**
+	 *  Opposite from diferenceA_BStringMovesTest()
+	 */
+	
+	String s12 = "banana";
+	String s22 = "nanbaa";
+	int result2 = 4;
 	
 	@Test
-	public void testString_mover2() {
+	public void oppositePriorityDirectionCharSwapTest() {
 		MinMovesInterface Minmoves =  new MinMovesSearchController();
 		assertEquals("It Must be = ", result2 , Minmoves.String_mover(s12, s22));
 		
 	}
+	
+	/**
+	 * Test greedy algorithm - shortest is better
+	 */
 	
 	String s13 = "aaaaaaaaab";
 	String s23 = "abaaaaaaaa";
@@ -35,34 +65,50 @@ public class MinMovesSearchControllerTest {
 	
 
 	@Test
-	public void testString_mover3() {
+	public void anotherSimpleGreedyAlgorithmTest() {
 		MinMovesInterface Minmoves =  new MinMovesSearchController();
 		assertEquals("It Must be = ", result3 , Minmoves.String_mover(s13, s23));
 		
 	}
 	
-	String s14 = "banana";
-	String s24 = "nanbaa";
-	int result4 = 4;
 	
+	/**
+	 * Test 
+	 * if (correctCharIndex.isEmpty()
+							|| !correctCharIndex.contains(swapIndex)) {
+						correctCharIndex.add(swapIndex);
+					} else {
+						if (workingString.charAt(charIndex) != inputString2
+								.charAt(charIndex))
+							charIndex = workingString.length() - 1;
 
+					}
+	 */
+	
+	
+	String s14 = "caaaaaaaab";
+	String s24 = "cbaaaaaaaa";
+	int result4 = 3;
+	
+	
 	@Test
-	public void testString_mover4() {
+	public void ruinedCorrectCharPositionTest() {
 		MinMovesInterface Minmoves =  new MinMovesSearchController();
 		assertEquals("It Must be = ", result4 , Minmoves.String_mover(s14, s24));
 		
 	}
-	
-	
-	
-	
+		
+	/**
+	 * Simple performace test - result is not confirmed on whiteboard !!
+	 */
 	
 	String s15 = "mfrhinffffjospriilft";
 	String s25 = "rponffsffijhmfilfitr";
 	int result5 = 52;
 	
+	
 	@Test
-	public void testString_mover5() {
+	public void someRandomLetterPerformaceTest() {
 		MinMovesInterface Minmoves =  new MinMovesSearchController();
 		assertEquals("It Must be = ", result5 , Minmoves.String_mover(s15, s25));
 		
